1e06000a347ea2a853c6d920cb7c3058b4e9389c
sawyeremily997 <109232639+sawyeremily997@users.noreply.github.com>
2023-10-18 21:45:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function instaClickFunction() {
+    window.open("https://www.instagram.com/emilyelisart/", '_blank');
+}
+
+function linkedinClickFunction() {
+    window.open("https://www.linkedin.com/in/sawyeremily/", '_blank');
+}
+
+function resumeFunction() {
+    window.location = "resume.pdf";
+}
